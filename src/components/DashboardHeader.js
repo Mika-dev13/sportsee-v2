845e46e1a61a1baee0e713e9e3a80e0bea61e6cf
Chorro Michael <michaelchorro.dev@gmail.com>
2022-08-18 10:41:01 +0200

    intégration dashboard header

--- src/components/DashboardHeader.js
+++ src/components/DashboardHeader.js
@@ -1,7 +1,9 @@
+import '../styles/DashboardHeader.css'
 export default function DashboardHeader({ firstName }) {
   return (
     <div>
-      <h1>Bonjour {firstName}</h1>
+      <h1>Bonjour <span className='firstname'>{firstName}</span></h1>
+      
       <p>Félicitation ! Vous avez explosez votre objectif hier 👏</p>
     </div>
   );
